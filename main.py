884c10dec1b751fad396815405f8efb70ec37ab5
Megumi Hirao <komegi.blog@gmail.com>
2024-04-18 23:09:18 +0900

    finished weather API with images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
-import backend import get_data
+from backend import get_data
+
+
 
 st.title("Weather forecast for the Next Days")
 place = st.text_input("Place:")
@@ -9,10 +11,16 @@ days = st.slider("Forcast Days", min_value=1, max_value=5, help="Select the numb
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    filtered_data= get_data(place, days)
 
-
-d, t = get_data(days)
-
-figure = px.line(x=d, y=t, labels= {"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temperatures, labels= {"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(figure)
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png", "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_paths = [images[condition] for condition in sky_condition]
+        st.image(image_paths, width=115)
